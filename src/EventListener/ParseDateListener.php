e32b40cfd2a90de73c767e821abe59bc7a9314a9
Hamid Abbaszadeh <abbaszadeh.h@gmial.com>
2023-05-21 19:51:54 +0330

    Prettifing  Code

--- src/EventListener/ParseDateListener.php
+++ src/EventListener/ParseDateListener.php
@@ -15,43 +15,22 @@ class ParseDateListener
 {
     public function __invoke(string $formattedDate, string $format, ?int $timestamp): string
     {
-        // Modify or create your own formatted date …
 
-        if ($GLOBALS['TL_LANGUAGE'] == 'fa') {
-			if ($timestamp === null)
-			{
-				$strDate = jDateTime::date($format);
-			}
-			else
-			{
-				$strDate = jDateTime::date($format, $timestamp);
-			}
-		} else {
-			$strDate = $formattedDate;
-		}
+        if ($GLOBALS['TL_LANGUAGE'] !== 'fa') {
+            return $formattedDate;
+        }
+        
+        if ($timestamp === null)
+        {
+            $strDate = jDateTime::date($format);
+        }
+        else
+        {
+            $strDate = jDateTime::date($format, $timestamp);
+        }
+		
 
 		return $strDate;
-        
-
-        /*
-        if ($GLOBALS['TL_LANGUAGE'] == 'fa' || $GLOBALS['TL_LANGUAGE'] == 'fa_IR') {
-
-            $formatter = new IntlDateFormatter(
-                "fa_IR@calendar=persian", 
-                IntlDateFormatter::FULL, 
-                IntlDateFormatter::FULL, 
-                'Asia/Tehran', 
-                IntlDateFormatter::TRADITIONAL, 
-                $format);
-
-            return $formatter->format($timestamp);
-
-        } else {
-
-            return $formattedDate;
-
-        }*/
-
         
     }
 }
